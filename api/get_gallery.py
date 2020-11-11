b0517517be85a355f24b66177026abbac9ce5bcf
anretsky <anretsky@umich.edu>
2020-11-11 13:15:17 -0500

    add get-gallery, get-user-info, publish apis

---
+++ api/get_gallery.py
@@ -0,0 +1,25 @@
+import json
+from dblib import *
+from s3lib import imgURL as signedURL
+
+def image_object(img):
+    return {
+        "imageUrl": signedURL(img['drawingId'])
+        "title": img['title']
+    }
+    
+def get_gallery(event, context):
+    canvases = fetch_gallery_canvases()
+    canvases = sorted(canvases, key = lambda i: i['modified'],reverse=True)
+    canvases = map(image_object, canvases)
+    templates = fetch_gallery_coloringPages()
+    templates = sorted(templates, key = lambda i: i['modified'],reverse=True)
+    templates = map(image_object, templates)
+    response = {
+        "canvases": canvases,
+        "templates": templates
+    }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
