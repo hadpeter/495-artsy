f01921fb653e0c9c2220f18cd672982e867749ed
hadpeter <hadpeter@umich.edu>
2020-11-11 02:17:02 -0500

    s3 presigned url

---
+++ S3Lib/package/lambda_function.py
@@ -0,0 +1,36 @@
+import json
+import boto3
+import requests
+
+
+def lambda_handler(event, context):
+    # construct location based on the api call and s3 folder structure
+    location = 'arn:aws:s3:::artsy-bucket/backgrounds/svg/'
+    desired_object = '01.svg'
+    s3 = boto3.client('s3')
+    #contents = s3.list_objects_v2(Bucket=location)
+    url = s3.generate_presigned_url('get_object', Params = {'Bucket': location, 'Key': desired_object}, ExpiresIn = 100)
+    #url = create_presigned_url('BUCKET_NAME', '')
+    if url is not None:
+        response = requests.get(url)
+        print("Got response")
+        print(response)
+    
+    #print(contents)
+    print("results")
+    # generate signed url
+    signed_url = "testing"
+    return {
+        'statusCode': 200,
+        'body': json.dumps(signed_url)
+    }
+    
+def upload_file():
+    object_name = 'OBJECT_NAME'
+    response = create_presigned_post('BUCKET_NAME', object_name)
+    if response is None:
+        exit(1)
+    with open(object_name, 'rb') as f:
+        files = {'file': (object_name, f)}
+        http_response = requests.post(response['url'], data=response['fields'], files=files)
+    logging.info(f'http status code: {http_response.status_code}')
