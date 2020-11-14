991f94fca45e106c3b3119b95a9d6f5eddfa75af
Edwin Marples <emarples@umich.edu>
2020-11-14 00:07:09 -0500

    all api enpoints work from api endpoint tester

--- package/apiLib.py
+++ package/apiLib.py
@@ -3,12 +3,18 @@ import boto3
 from dblib import *
 import time
 import s3_lib
-
+import random #TODO get rid of random calls
 
 def get_user_info(event):
-	userId = event['headers']['userId']
+    userId = event['headers']['userId']
     attrs = ["userId", "coins", "brushes", "paints", "baseline", "breathCount", "backgrounds", "drawings", "unlimitedExpiration"]
     user_info = get_user_attr(userId, attrs)
+    
+    #fix number issues
+    user_info["coins"] = int(user_info["coins"])
+    user_info["breathCount"] = int(user_info["breathCount"])
+    user_info["unlimitedExpiration"] = int(user_info["unlimitedExpiration"])
+    user_info["baseline"] = int(user_info["baseline"])
     return {
         'statusCode': 200,
         'body': json.dumps(user_info)
@@ -16,41 +22,40 @@ def get_user_info(event):
 
 
 def get_user_art(event):
-	userId = event["queryStringParameters"]["userId"]
-	data = fetch_user_art_all(userId)
-	canvas = []
-	template = []
+    userId = event["headers"]["userId"]
+    data = fetch_user_art_all(userId)
+    canvas = []
+    template = []
 
-	for drawing in data:
-		new_item = {
-			"png": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawing["drawingId"]}.png'),
-			"svg": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawing["drawingId"]}.svg'),
-			"drawingId": drawing["drawingId"],
-			"time": drawing["modified"]
-		}
+    for drawing in data:
+        new_item = {
+            "png": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawing["drawingId"]}.png'),
+            "svg": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawing["drawingId"]}.svg'),
+            "drawingId": drawing["drawingId"],
+            "time": int(drawing["modified"])
+        }
 
-		if drawing["coloringPage"]:
-			new_item['templateUrl'] = s3_lib.get_file('artsy-bucket', f'backgrounds/{img['drawingId']}.png')
-			template.append(new_item)
-		else:
-			canvas.append(new_item)
+        if drawing["coloringPage"] != "":
+            new_item['templateUrl'] = s3_lib.get_file('artsy-bucket', f'backgrounds/svg/{drawing["coloringPage"]}.svg')
+            template.append(new_item)
+        else:
+            canvas.append(new_item)
 
-	response = {
+    response = {
         "canvas": canvas,
         "template": template
     }
 
-	return {
-		'statusCode': 200
-		'body': json.dumps(response)
-	}
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
 
 
-def create_user(event):
-	userId = event["queryStringParameters"]['deviceId'] + "-" + str(time.time_ns())
-    baseline = { "flow":10, "volume":20 }
-    create_user(userId, baseline, int(time.time()) )
-
+def api_create_user(event):
+    
+    userId = event["headers"]["deviceId"] + "-" + str(time.time_ns())
+    create_user(userId)
     return {
         'statusCode': 200,
         'body': json.dumps(userId)
@@ -58,15 +63,15 @@ def create_user(event):
 
 
 def get_drawing(event):
-	drawingId = event["queryStringParameters"]['drawingId']
-	userId = img['drawingId'].split('-')[0]
+    drawingId = event["headers"]['drawingId']
+    userId = event['headers']['drawingId'].split('-')[0]
 
-	response = {
-		"png": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawingId}.png'),
-		"svg": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawingId}.svg')
-	}
+    response = {
+        "png": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawingId}.png'),
+        "svg": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{drawingId}.svg')
+    }
 
-	return {
+    return {
         'statusCode': 200,
         'body': json.dumps(response)
     }
@@ -79,29 +84,39 @@ def get_templates(event):
         'body': json.dumps(templates)
     }
 
-
-def create_drawing(event):
-	create_drawing(event['userId'],create_id(event['userId']),event['template'],time.time_ns())
+def api_create_drawing(event):
+    drawingId = create_id(event['headers']['userId'])
+    if 'template'in event['headers']:
+        create_drawing(event['headers']['userId'], drawingId ,event['headers']['template'],time.time_ns())
+    else:
+        create_drawing(event['headers']['userId'], drawingId, "", time.time_ns())
     return {
         'statusCode': 200,
-        'body': str(type(drawingIds))
+        'body': json.dumps(drawingId)
     }
 
 def save_drawing(event):
-	response = s3_lib('artsy-bucket',event['drawingId'])
-    set_title(event['drawingId'],event['title'])
+    drawingId = event["headers"]['drawingId']
+    userId = event["headers"]['drawingId'].split('-')[0]
+    
+    if "title" in event["headers"]:
+        set_title(event["headers"]['drawingId'],event["headers"]['title'])
+
+    response = {
+        "png": s3_lib.upload_file('artsy-bucket', f'drawings/{userId}/{drawingId}.png'),
+        "svg": s3_lib.upload_file('artsy-bucket', f'drawings/{userId}/{drawingId}.svg')
+    }
+    
     return {
         'statusCode': 200,
-        'body': json.dumps(response),
-        'png':str(event['drawingId'])+".png",
-        'svg': str(event['drawingId'])+".svg"
+        'body': json.dumps(response)
     }
 
 
 def purchase_brush(event):
     userId = event['headers']['userId']
     brushId = event['headers']['brushId']
-    cost = event['headers']['cost']
+    cost = int(event['headers']['cost'])
     add_coins(userId, cost*(-1))
     add_brush(userId, brushId)
     return {
@@ -110,9 +125,9 @@ def purchase_brush(event):
 
 
 def purchase_paint(event):
-	userId = event['headers']['paintId']
+    userId = event['headers']['userId']
     paintId = event['headers']['paintId']
-    cost = event['headers']['cost']
+    cost = int(event['headers']['cost'])
     add_coins(userId, cost*(-1))
     add_paint(userId, paintId)
     return {
@@ -121,9 +136,9 @@ def purchase_paint(event):
 
 
 def purchase_background(event):
-	userId = event['headers']['userId']
+    userId = event['headers']['userId']
     backgroundId = event['headers']['backgroundId']
-    cost = event['headers']['cost']
+    cost = int(event['headers']['cost'])
     add_coins(userId, cost*(-1))
     add_background(userId, backgroundId)
     return {
@@ -131,15 +146,15 @@ def purchase_background(event):
     }
 
 def get_gallery(event):
-	canvases = fetch_gallery_canvases()
+    canvases = fetch_gallery_canvases()
     canvases = sorted(canvases, key = lambda i: i['modified'],reverse=True)
     canvases = map(image_object, canvases)
     templates = fetch_gallery_coloringPages()
     templates = sorted(templates, key = lambda i: i['modified'],reverse=True)
     templates = map(image_object, templates)
     response = {
-        "canvases": canvases,
-        "templates": templates
+        "canvases": list(canvases),
+        "templates": list(templates)
     }
     return {
         'statusCode': 200,
@@ -148,7 +163,7 @@ def get_gallery(event):
 
 
 def publish_to_gallery(event):
-	drawingId = event['headers']['drawingId']
+    drawingId = event['headers']['drawingId']
     title = event['headers']['title']
     set_title(drawingId, title)
     publish_drawing(drawingId)
@@ -156,28 +171,32 @@ def publish_to_gallery(event):
         'statusCode': 200
     }
 
-def add_breath(event):
-	score = compute_score(event['flow'],event['volume'])
-    baseline = get_user_attr(event['userId'], ["baseline"])['baseline']
+def api_add_breath(event):
+    score = compute_score(event['headers']['flow'],event['headers']['volume'])
+    baseline = get_user_attr(event['headers']['userId'], ["baseline"])['baseline']
     #LOWBAR = 0.4
     LOWBAR = 0 #TEST VALUE
     #UNLIMITED_DURATION = 3600000000000 #one hour in nanoseconds
     UNLIMITED_DURATION = 10000000000 #10 seconds in nanoseconds | TEST VALUE
     
     if(score > LOWBAR*baseline):
-        if(get_user_attr(event['userId'],['unlimitedExpiration'])['unlimitedExpiration']<time.time_ns()):
-            add_breath(event['userId'])
-            if(get_user_attr(event['userId'],['breathCount'])['breathCount']==10):
-                set_unlimited(event['userId'], time.time_ns()+UNLIMITED_DURATION)
-        add_coins(event['userId'],score)
+        if(get_user_attr(event['headers']['userId'],['unlimitedExpiration'])['unlimitedExpiration']<time.time_ns()):
+            add_breath(event['headers']['userId'])
+            if(get_user_attr(event['headers']['userId'],['breathCount'])['breathCount']==10):
+                set_unlimited(event['headers']['userId'], time.time_ns()+UNLIMITED_DURATION)
+        add_coins(event['headers']['userId'],score)
     if(score > baseline):
-        set_baseline(event['userId'],score)
+        set_baseline(event['headers']['userId'],score)
+        
+    response = {
+        'seconds_until_use': int((get_user_attr(event['headers']['userId'],['unlimitedExpiration'])['unlimitedExpiration']-time.time_ns())/1000000000),
+        'balance': int(get_user_attr(event['headers']['userId'],['coins'])['coins']),
+        'breathCount': int(get_user_attr(event['headers']['userId'],['breathCount'])['breathCount'])
+    }
     
     return {
         'statusCode': 200,
-        'seconds until use': (get_user_attr(event['userId'],['unlimitedExpiration'])['unlimitedExpiration']-time.time_ns())/1000000000,
-        'balance': get_user_attr(event['userId'],['coins'])['coins'],
-        'breathCount': get_user_attr(event['userId'],['breathCount'])['breathCount']
+        'body': json.dumps(response)
     }
 
 
@@ -187,27 +206,30 @@ def create_id(userId):
 
 def image_object(img):
     userId = img['drawingId'].split('-')[0]
-    return {
-        "imageUrl": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{img['drawingId']}.png')
+
+    response = {
+        "imageUrl": s3_lib.get_file('artsy-bucket', f'drawings/{userId}/{img["drawingId"]}.png'),
         "title": img['title']
     }
+    
+    return response
 
 def compute_score(flow,volume):
     #TODO implement something that actually analyses volume and flow
     return random.randint(50,50)
 
 apiDict = {
-	"get-user-info": get_user_info,
-	"get-user-art": get_user_art,
-	"create-user": create_user,
-	"get-drawing": get_drawing,
-	"get-templates": get_templates,
-	"create-drawing": create_drawing,
-	"save-drawing": save_drawing,
-	"purchase-brush": purchase_brush,
-	"purchase-paint": purchase_paint,
-	"purchase-background": purchase_background,
-	"get-gallery": get_gallery,
-	"publish-to-gallery": publish_to_gallery,
-	"add-breath": add_breath
+    "get-user-info": get_user_info,
+    "get-user-art": get_user_art,
+    "create-user": api_create_user,
+    "get-drawing": get_drawing,
+    "get-templates": get_templates,
+    "create-drawing": api_create_drawing,
+    "save-drawing": save_drawing,
+    "purchase-brush": purchase_brush,
+    "purchase-paint": purchase_paint,
+    "purchase-background": purchase_background,
+    "get-gallery": get_gallery,
+    "publish-to-gallery": publish_to_gallery,
+    "add-breath": api_add_breath
 }
